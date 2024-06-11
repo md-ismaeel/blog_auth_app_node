8d2025691475793b8bc70d6f8b430aaa9bbffadf
Md Ismail <mdismaeelkhan345@gmail.com>
2024-06-11 19:55:31 +0530

    updation

--- MIddleware/authMiddleware.js
+++ MIddleware/authMiddleware.js
@@ -3,75 +3,105 @@ require('dotenv').config()
 
 const authModel = require('../Model/model.auth')
 
+const passport = require('passport');
+
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+
 const jwtKey = process.env.JTWKEY;
 
-const validateUsers = async (req, res, next) => {
-    const headers = req.headers;
-    const tokenFromHeaders = headers.authorization.split(" ")[1];
-    // console.log(tokenFromHeaders);
-    /**
-     * Points to be validated in token
-     * 1. Token should be present
-     * 2. Secret key validation (This is the same token that we have generated)
-     * 3. Token expiry date should not be passed
-     * 4. Validate the issued at date (Optional)
-     * 5. Validate the user id if it is present in database
-     */
+// const validateUsers = async (req, res, next) => {
+//     const headers = req.headers;
+//     const tokenFromHeaders = headers.authorization.split(" ")[1];
+//     // console.log(tokenFromHeaders);
+//     /**
+//      * Points to be validated in token
+//      * 1. Token should be present
+//      * 2. Secret key validation (This is the same token that we have generated)
+//      * 3. Token expiry date should not be passed
+//      * 4. Validate the issued at date (Optional)
+//      * 5. Validate the user id if it is present in database
+//      */
 
-    // 1
-    if (!tokenFromHeaders) {
-        return res.status(401).json({
-            message: "Unauthenticated user"
-        })
+//     // 1
+//     if (!tokenFromHeaders) {
+//         return res.status(401).json({
+//             message: "Unauthenticated user"
+//         })
+//     }
+
+//     //2
+//     try {
+//         jwt.verify(tokenFromHeaders, jwtKey)
+//     } catch (err) {
+//         return res.status(401).json({
+//             message: "Unauthenticated user"
+//         })
+//     }
+
+//     // //3
+//     const tokenData = jwt.decode(tokenFromHeaders);
+//     // console.log(tokenData);
+
+//     const tokenExp = tokenData.exp;
+//     const current = Math.ceil(new Date().getTime() / 1000);
+
+//     if (current > tokenExp) {
+//         return res.status(401).json({
+//             message: "Unauthenticated user"
+//         })
+//     }
+
+//     //4 
+//     const tokenIat = tokenData.iat;
+//     const maxTokenAge = 60 * 60 * 27 * 7;
+
+//     if (current - tokenIat > maxTokenAge) {
+//         return res.status(401).json({ message: "Token too old" });
+//     }
+
+
+//     //5 
+//     const userId = tokenData.userId;
+//     try {
+//         const user = await authModel.findById(userId);
+//         if (!user) {
+//             return res.status(401).json({
+//                 message: "Unauthenticated user"
+//             })
+//         }
+//         req.user = user;
+//     } catch (err) {
+//         res.json({
+//             message: 'Internal Server Error'
+//         })
+//     }
+//     next() // after clear all check proceed to nextMiddleware
+// }
+
+var opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtKey;
+console.log(opts);
+
+const strategy = new JwtStrategy(opts, async function (jwt_payload, done) {
+    const userId = jwt_payload.userId;
+    const user = await authModel.findById(userId);
+
+    if (!user) {
+        return done("Invalid user", false);
     }
 
-    //2
-    try {
-        jwt.verify(tokenFromHeaders, jwtKey)
-    } catch (err) {
-        return res.status(401).json({
-            message: "Unauthenticated user"
-        })
+    if (user) {
+        return done(null, user);
+    } else {
+        return done(null, false);
+        // or you could create a new account
     }
 
-    // //3
-    const tokenData = jwt.decode(tokenFromHeaders);
-    // console.log(tokenData);
+});
 
-    const tokenExp = tokenData.exp;
-    const current = Math.ceil(new Date().getTime() / 1000);
+passport.use(strategy);
 
-    if (current > tokenExp) {
-        return res.status(401).json({
-            message: "Unauthenticated user"
-        })
-    }
-
-    //4 
-    const tokenIat = tokenData.iat;
-    const maxTokenAge = 60 * 60 * 27 * 7;
-
-    if (current - tokenIat > maxTokenAge) {
-        return res.status(401).json({ message: "Token too old" });
-    }
-
-
-    //5 
-    const userId = tokenData.userId;
-    try {
-        const user = await authModel.findById(userId);
-        if (!user) {
-            return res.status(401).json({
-                message: "Unauthenticated user"
-            })
-        }
-        req.user = user;
-    } catch (err) {
-        res.json({
-            message: 'Internal Server Error'
-        })
-    }
-    next() // after clear all check proceed to nextMiddleware
-}
-
-module.exports = validateUsers;
+module.exports = passport;
--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
+
+const cors = require("cors");
+const { rateLimit } = require("express-rate-limit");
+const MongoStore = require("rate-limit-mongo");
 
 const authRouter = require('./Routes/authroute');
-const postRouter = require('./Routes/postroutes')
+const postRouter = require('./Routes/postroutes');
 
-const authMiddleware = require('./MIddleware/authMiddleware')
+const passport = require('./MIddleware/authMiddleware');
+// const authMiddleware = require('./MIddleware/authMiddleware')
 
 const app = express();
 
 const port = process.env.PORT;
-const mongoDbConnection = process.env.MONGODBCONNECTION
+const mongoDbConnection = process.env.MONGODBCONNECTION;
+
+const atlasUser = process.env.USER;
+const atlasPassword = process.env.PASSWORD;
+
+const corsOptions = {
+    origin: "https://blogapp-kqpz.onrender.com",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+const windowMs = 30 * 60 * 1000;
+
+const limiter = rateLimit({
+    store: new MongoStore({
+        uri: mongoDbConnection,
+        user: atlasUser,
+        password: atlasPassword,
+        // should match windowMs
+        expireTimeMs: windowMs,
+        errorHandler: console.error.bind(null, "rater-limit-mongo"),
+        // see Configuration section for more options and details
+    }),
+    windowMs: windowMs, // 5 minutes
+    limit: 4, // Limit each IP to 25 requests per `window` (here, per 15 minutes).
+});
 
 
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(limiter);
+
 
 mongoose.connect(mongoDbConnection)
     .then(() => console.log('mongoDb connected successfully'))
     .catch((err) => console.log('Error, db not connected', err))
 
-app.use('/api/v1/', authRouter)
-app.use('/api/v1/post', authMiddleware, postRouter)
+
+app.use('/api/v1/', authRouter);
+app.use('/api/v1/post', passport.authenticate("jwt", { session: false }), postRouter);
+
 
 app.use('/*', (req, res) => {
     res.status(404).json({
@@ -28,6 +62,5 @@ app.use('/*', (req, res) => {
         message: "Path not found"
     })
 })
-
 
 app.listen(port, () => console.log(`Server is up and running on port ${port}`))
